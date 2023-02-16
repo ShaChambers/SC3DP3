7f434d43e6cede1dcd60e1a68765ef967e12644d
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-16 15:16:49 -0500

    Remove unneeded code

--- client/src/components/HistoryList/index.js
+++ client/src/components/HistoryList/index.js
@@ -12,38 +12,17 @@ const HistoryList = ({
     return <h3>Nothing here... Yet</h3>;
   }
 
+  // _id
+  //     description
+  //     person
+  //     link
+
   return (
     <div>
-      {/* {showPerson && <h3>{person}</h3>} */}
       {history &&
         history.map((history) => (
           <div key={history._id} className="card mb-3">
             <h3>Hello</h3>
-            {/* <h4 className="card-header bg-primary text-light p-2 m-0">
-              {showPerson ? (
-                <Link className="text-light" to={`/people/${history.person}`}>
-                  {history.person} <br />
-                  <span style={{ fontSize: "1rem" }}>
-                    {history.description}
-                  </span>
-                </Link>
-              ) : (
-                <>
-                  <span style={{ fontSize: "1rem" }}>
-                    {history.description}
-                  </span>
-                </>
-              )}
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{history.description}</p>
-            </div>
-            <Link
-              className="btn btn-primary btn-block btn-squared"
-              to={`/history/${history._id}`}
-            >
-              Placeholder Text
-            </Link> */}
           </div>
         ))}
     </div>
